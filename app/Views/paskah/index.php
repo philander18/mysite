2a8554631c47b4d6e7ea9363e8c7325d8a706833
philander18 <philander.totalis@gmail.com>
2024-03-01 17:59:33 +0700

    Membuat form pendaftaran

--- app/Views/paskah/index.php
+++ app/Views/paskah/index.php
@@ -1,6 +1,57 @@
 <?= $this->extend('paskah/template/index'); ?>
 <?= $this->section('page-content'); ?>
 <div class="container-fluid paskah" style="height: 100vh;">
-
+    <div class="container">
+        <form>
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-8 col-lg-6">
+                    <div class="text-dark mb-3 fw-bold">
+                        <h3 class="fw-bold text-center">PENDAFTARAN PASKAH</h3>
+                        <h4 class="text-center">GPdI Padalarang 2024</h4>
+                    </div>
+                    <div class="mb-3">
+                        <input class="form-control" type="text" id="nama" name="nama" placeholder="Nama">
+                    </div>
+                    <div class="mb-3">
+                        <input class="form-control" type="text" id="HP" name="HP" placeholder="Nomor HP">
+                    </div>
+                    <div class="mb-3">
+                        <textarea class="form-control" id="anggota" rows="3" placeholder="Daftar anggota"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select" aria-label=".form-select-sm example" name="transportasi" id="transportasi">
+                            <option selected>Pribadi</option>
+                            <option value="panitia">Panitia</option>
+                            <option value="pribadi">Pribadi</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-4">
+                                <label class="text-dark fw-bold" for="dewasa">Jumlah Dewasa</label>
+                            </div>
+                            <div class="col-8">
+                                <input class="form-control" type="text" id="dewasa" name="dewasa" placeholder="Jumlah Dewasa">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-4">
+                                <label class="text-dark fw-bold text-start" for="anak">Jumlah Anak</label>
+                            </div>
+                            <div class="col-8">
+                                <input class="form-control" type="text" id="anak" name="anak" placeholder="jumlah Anak">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-light text-dark fw-bold">Submit</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+</form>
 </div>
 <?= $this->endSection(); ?>
--- app/Views/paskah/template/index.php
+++ app/Views/paskah/template/index.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $judul; ?></title>
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Custom fonts for this template-->
     <link href="<?= base_url(); ?>vendor/fontawesome-free-6.5.1-web/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="<?= base_url(); ?>public/css/styles.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- bootstrap dan datatables-->
     <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>vendor/DataTables/Bootstrap-5-5.3.0/css/bootstrap.css">
